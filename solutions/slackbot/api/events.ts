4a0869a36b092932b263ca17efa3355670d59351
AvsheshSavani7 <138287046+AvsheshSavani7@users.noreply.github.com>
2024-10-21 17:30:00 +0530

    update-event file

--- solutions/slackbot/api/events.ts
+++ solutions/slackbot/api/events.ts
@@ -31,7 +31,8 @@ export async function POST(request: Request) {
     if (requestType === 'event_callback') {
       const eventType = body.event.type
       if (eventType === 'app_mention') {
-        await sendGPTResponse(body.event)
+        // Acknowledge the event immediately
+        sendGPTResponse(body.event).catch((error) => console.error(error))
         return new Response('Success!', { status: 200 })
       }
     }
