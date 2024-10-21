588a2cee6ff35d9253ab7267f1b1cb36f2f8d511
AvsheshSavani7 <138287046+AvsheshSavani7@users.noreply.github.com>
2024-10-21 18:07:07 +0530

    update-questiojns

--- solutions/slackbot/api/_chat.ts
+++ solutions/slackbot/api/_chat.ts
@@ -55,7 +55,7 @@ export async function sendGPTResponse(event: Event) {
     const apiResponse = await sendHTTPRequestUsingFetch(
       'http://lead-source-api.kasawalkthrough.com/api/lead/chat/db',
       {
-        question: prompts.map((prompt) => prompt.content).join(' '),
+        question: prompts[0]?.content,
       }
     )
 
